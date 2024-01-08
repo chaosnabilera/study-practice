ec91f92701234630cdc421ec83e88c65a1c58a79
chaosnabilera <chaosnabilera7@gmail.com>
2024-01-08 21:09:03 +0900

    section 5 going modular done

---
+++ pytorch_ztm_2023/06_going_modular/going_modular/data_setup.py
@@ -0,0 +1,69 @@
+
+# Jupyter notebook Script 모드로 사용하기 위해 %%writefile magic command 사용
+# %%writefile magic command는 반드시 첫줄이어야 한다
+
+"""
+Contains functionality for creating PyTorch DataLoaders for 
+image classification data.
+"""
+import os
+
+import torchvision
+import torch.utils.data
+
+NUM_WORKERS = os.cpu_count()
+
+def create_dataloaders(
+    train_dir: str, 
+    test_dir: str, 
+    transform: torchvision.transforms.Compose, 
+    batch_size: int, 
+    num_workers: int=NUM_WORKERS
+):
+  """Creates training and testing DataLoaders.
+
+  Takes in a training directory and testing directory path and turns
+  them into PyTorch Datasets and then into PyTorch DataLoaders.
+
+  Args:
+    train_dir: Path to training directory.
+    test_dir: Path to testing directory.
+    transform: torchvision transforms to perform on training and testing data.
+    batch_size: Number of samples per batch in each of the DataLoaders.
+    num_workers: An integer for number of workers per DataLoader.
+
+  Returns:
+    A tuple of (train_dataloader, test_dataloader, class_names).
+    Where class_names is a list of the target classes.
+    Example usage:
+      train_dataloader, test_dataloader, class_names = \
+        = create_dataloaders(train_dir=path/to/train_dir,
+                             test_dir=path/to/test_dir,
+                             transform=some_transform,
+                             batch_size=32,
+                             num_workers=4)
+  """
+  # Use ImageFolder to create dataset(s)
+  train_data = torchvision.datasets.ImageFolder(train_dir, transform=transform)
+  test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
+
+  # Get class names
+  class_names = train_data.classes
+
+  # Turn images into data loaders
+  train_dataloader = torch.utils.data.DataLoader(
+      train_data,
+      batch_size=batch_size,
+      shuffle=True,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+  test_dataloader = torch.utils.data.DataLoader(
+      test_data,
+      batch_size=batch_size,
+      shuffle=False, # don't need to shuffle test data
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+
+  return train_dataloader, test_dataloader, class_names
